1ed57462a778af727b69bffb98f243983f649c4a
dt9650329-ops <dt9650329@gmail.com>
2026-08-05 21:50:04 -0500

    Implement battery optimization exemption request
    Add battery optimization request in MainActivity.

--- capacitor-app/android/app/src/main/java/com/servialiados/repartidor/MainActivity.java
+++ capacitor-app/android/app/src/main/java/com/servialiados/repartidor/MainActivity.java
@@ -1,5 +1,30 @@
 package com.servialiados.repartidor;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        solicitarExencionBateria();
+    }
+
+    private void solicitarExencionBateria() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+            String packageName = getPackageName();
+            if (pm != null && !pm.isIgnoringBatteryOptimizations(packageName)) {
+                Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + packageName));
+                startActivity(intent);
+            }
+        }
+    }
+}
